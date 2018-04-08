f96ddb6c640d0aee3ba4e109dc5752f2daef06ea
unknown <youssefswailem@gmail.com>
2018-04-08 20:27:09 +0200

    Remove unused dependencies

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,5 @@
-import Link from 'next/link'
-import Head from 'next/head'
-import React, { Component } from 'react';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-
-import Bar from '../components/Bar';
-import SideMenu from '../components/SideMenu';
 import VideoGrid from '../components/VideoGrid'
-import { Channel } from "./channel";
-
-import { BrowserRouter } from 'react-router-dom';
-import { matchRoutes, renderRoutes } from 'react-router-config';
-
 import Wrapper from '../components/Wrapper';
-
-
-
-
-
-
-
 
 const Home = (props) => (
     <Wrapper>
